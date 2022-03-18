92266c6fd16d1139972fe61277ed7e19a1d9b187
iqball1 <100454265+iqball1@users.noreply.github.com>
2022-03-18 17:23:30 +0700

    Add files via upload

---
+++ config/FIREBASE/index.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBfEOsB8f4Gy-WorsKHjYIgzvDN_DUQH-0",
+  authDomain: "kantinku-2f934.firebaseapp.com",
+  databaseURL: "https://kantinku-2f934-default-rtdb.firebaseio.com",
+  projectId: "kantinku-2f934",
+  storageBucket: "kantinku-2f934.appspot.com",
+  messagingSenderId: "480440407229",
+  appId: "1:480440407229:web:cae77b5315ff27d54b13d4"
+});
+
+const FIREBASE = firebase;
+
+export default FIREBASE;
